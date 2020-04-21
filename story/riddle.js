5f6f2b98d88770720931ce4ce146c48707fd9767
Philipp Möller <philipp@kinscher-moeller.de>
2020-04-21 02:04:30 +0200

    add huffman coding riddle

--- story/riddle.js
+++ story/riddle.js
@@ -2,3 +2,6 @@ const COLORS_STR = ['blau','weiß','rot','gelb','grün','rosa','grau','orange','
 const COLORS_HEX = ['#0000e9','#ffffff','#cf0000','#ffca00','#00be00','#ffacf7','#bcbcbc','#ff6000','#6000bf','#000000']
 
 const ASCII_HEX_SOLUTION = 350; // use only letters with ascii-chars
+
+const HUFFMAN_CAESAR = 'DER SCHLUESSEL ZU DIESEM RAETSEL IST DIE LAENGE DIESER NACHRICHT KODIERT MIT DER METHODE VON DAVID A HUFFMAN ANGEGEBEN IN DER KLEINSTEN EINHEIT FUER DIE DATENMENGE DIGITAL REPRAESENTIERTER DATEN';
+const HUFFMAN_CAESAR_SHIFT = 17;
